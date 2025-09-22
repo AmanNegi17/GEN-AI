6ff6b42186f093ebc4549a92e406c57b3652cb6f
Aman <Aman@amansinghnegi655.com>
2025-09-22 22:14:32 +0530

    first

--- src/lib/models/otp.ts
+++ src/lib/models/otp.ts
@@ -1,20 +1,20 @@
-import mongoose, { Schema, models } from "mongoose";
+// // import mongoose, { Schema, models } from "mongoose";
 
-const otpSchema = new Schema({
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  otp: {
-    type: String,
-    required: true,
-  },
-  otpGeneratedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+// const otpSchema = new Schema({
+//   identifier: {
+//     type: String,
+//     required: true,
+//     unique: true, // mobile or email
+//   },
+//   otp: {
+//     type: String,
+//     required: true,
+//   },
+//   otpGeneratedAt: {
+//     type: Date,
+//     default: Date.now,
+//   },
+// });
 
-const Otp = models.Otp || mongoose.model("Otp", otpSchema);
-export default Otp;
+// const Otp = models.Otp || mongoose.model("Otp", otpSchema);
+// export default Otp;
